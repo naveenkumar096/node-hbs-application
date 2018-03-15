298dee5d66329843214198c7d1f1edb922484de7
ODC-HB3-WD047\Naveen <naveen.julla@iconma.com>
2018-03-15 19:14:10 +0530

    changes on server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const hbs = require('hbs');
+const port=process.env.PORT || 3000;
 
 var app = express();
+app.use((req,res,next)=>{
+  next();
+})
+{
+
+}
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
@@ -40,7 +47,7 @@ app.get('/about',(req,res)=>{
     });
 });
 
-app.listen(3000,()=>{
-console.log('server is starting port 3000');
+app.listen(port,()=>{
+console.log('server is starting port '+port);
 
 });
